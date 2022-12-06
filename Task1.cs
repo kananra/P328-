83f928ca060886cda966f625be82d028c12e1eb7
kananra <kananara@code.edu.az>
2022-12-07 00:31:29 +0400

    task

---
+++ Task1.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Zehmet olmasa reqem daxil edin");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n>0&&n%3==0&&n%7==0) 
+{
+    Console.WriteLine("Bu eded hem 3 hemde 7e bolunur") ;
+
+}
+else
+{
+    Console.WriteLine("Bu eded hec birine bolunmur");
+}
---
+++ Task2.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Zehmet olmasa reqem daxil edin");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n >= 0 && n >= 91&& n <= 100 )
+{
+    Console.WriteLine("A");
+}
+else if (n >= 81 && n <= 90)
+{
+    Console.WriteLine("B");
+}
+else if (n >= 71 && n <= 80)
+{
+    Console.WriteLine("C");
+}
+else if (n >= 61 && n <= 70)
+{
+    Console.WriteLine("D");
+}
+else if (n >= 51 && n <= 60)
+{
+    Console.WriteLine("E");
+}
+else if (n < 51)
+{
+    Console.WriteLine("Kesildiniz");
+}
+else
+{
+    Console.WriteLine("Reqem duzgun daxil edilmeyib");
+}
