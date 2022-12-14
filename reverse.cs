f5440a9bb51e53034a03451df11133eae6820464
kananra <118839498+kananra@users.noreply.github.com>
2022-12-14 13:12:12 +0400

    created

---
+++ reverse.cs
@@ -0,0 +1,16 @@
+﻿
+string word = "Hello World";
+string word1 = string.Empty;
+
+for (int i = word.Length - 1; i >= 0; i--)
+{
+    word1 += word[i];
+}
+
+string word2 = word1.Split(' ')[1];
+string word3 = word1.Split(' ')[0];
+
+
+Console.WriteLine($"{word2} {word3}");
+
+
