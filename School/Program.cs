1fe39ec43aec64f8bdf08120fda812ce4aa2ee8d
kananra <118839498+kananra@users.noreply.github.com>
2022-12-18 13:18:24 +0400

    Created

---
+++ School/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Please enter your Name,Surname,Group,Point");
+Student student = new Student(Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), int.Parse(Console.ReadLine()));
+student.NameSurname();
+student.GetFullInfo();
+student.Graduate();
---
+++ School/Student.cs
@@ -0,0 +1,63 @@
+﻿
+
+
+using System;
+using System.Drawing;
+
+class Student
+{
+    public string Name;
+    public string SurName;
+    public string Group;
+    public int Point;
+    public bool IsGraduated;
+
+
+    public Student(string name, string surName, string group, int point)
+    {
+
+        Name = name;
+        SurName = surName;
+        Group = group;
+        Point = point;
+        
+    }
+
+   
+    public void NameSurname()
+    {
+        Console.WriteLine($"Name: {Name} Surname: {SurName}");
+    }
+    public void GetFullInfo()
+    {
+        Console.WriteLine($"Name: {Name} Name: {SurName} Group: {Group} Point: {Point}");
+    }
+    public void Graduate()
+    {
+        if (Point >= 51)
+        {
+
+            if (IsGraduated = true)
+            {
+                Console.WriteLine("You are graduated");
+                if (Point > 80)
+                {
+                    Console.WriteLine("You can enter the test for a second time to increase your point");
+                }
+                else
+                {
+                    Console.WriteLine("You can not enter the test for a second time to increase your point");
+                }
+            }
+           
+           
+        }
+        else
+        {
+            Console.WriteLine("You are failed");
+        }
+      
+}
+    
+}
+
