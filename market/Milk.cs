532a8d5340f5255ee6e51a590fe58cb7d2167753
kananra <118839498+kananra@users.noreply.github.com>
2022-12-20 02:13:30 +0400

    Add files via upload

---
+++ market/Milk.cs
@@ -0,0 +1,16 @@
+﻿
+internal class Milk : Product
+{
+    public double Volume;
+    public double FatRate;
+
+    public Milk(int count, double volume, double fatRate)
+    {
+        this.Count = count;
+        Volume = volume;
+        FatRate = fatRate;
+    }
+
+   
+}
+
---
+++ market/Product.cs
@@ -0,0 +1,92 @@
+﻿
+internal class Product
+{
+    private string Name;
+    private int Price;
+    protected int Count;
+    private int TotalIncome;
+    private int Num;
+
+    public Product(int count)
+    {
+
+        Count = count;
+
+
+    }
+
+    public void MinPrice(int price)
+    {
+        if (price > 0)
+        {
+            Price = price;
+        }
+        else
+        {
+            while (price < 1)
+            {
+
+                Console.WriteLine("Price can not be less than 1");
+                Console.WriteLine("Please write again");
+                int less = int.Parse(Console.ReadLine());
+                price = less;
+
+            }
+        }
+    }
+
+    public void Null(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+        {
+            while (string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("Please enter your product name");
+                string check = Console.ReadLine();
+                name = check;
+            }
+
+
+        }
+        else
+        {
+            Name = name;
+        }
+
+
+    }
+    public int Sell(int num)
+    {
+        Num = num;
+        while (num == 1)
+        {
+            if (Count > 0)
+            {
+                Count--;
+                TotalIncome += Price;
+                Console.WriteLine("If you want to sell again press 1");
+                int newnum = int.Parse(Console.ReadLine());
+                num = newnum;
+            }
+            else
+            {
+                Console.WriteLine("Out of Stock");
+                Console.WriteLine($"Total income: {TotalIncome}$");
+                
+            }
+
+            
+        }
+        return Count;
+
+    }
+
+
+}
+
+
+
+
+
+
+
